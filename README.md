1e6b371ce47d820eb527c7f4f81dac37f15cc4e3
resinya <2970768980@qq.com>
2026-06-17 19:27:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello algo
+
+- description:此项目记录我于2026/06/17开启算法刷题之路
---
+++ baidu/21. 合并两个有序链表.js
@@ -0,0 +1,5 @@
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode(0);
+  let cur = dummy;
+};
+// 两个链表本身升序，新建虚拟头节点，用指针依次比较两链表当前节点，把更小的接到结果链表后，最后接上剩余未遍历完的链表
